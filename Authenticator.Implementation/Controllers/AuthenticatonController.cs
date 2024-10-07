236a70a0b4bd780a1fce5bf4e662b30717ce9e24
Abdul Moiz <moiza0799@gmail.com>
2024-10-07 11:46:12 -0500

    Code changes

---
+++ Authenticator.Implementation/Controllers/AuthenticatonController.cs
@@ -0,0 +1,43 @@
+﻿using Authenticator.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authenticator.Implementation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticatonController : ControllerBase
+    {
+        private readonly AuthenticationService _authenticationService;
+
+        public AuthenticatonController(AuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpGet("google-login")]
+        public IActionResult GoogleLogin()
+        {
+            var redirectUri = Url.Action("GoogleResponse");
+            return _authenticationService.GoogleLogin(redirectUri);
+        }
+
+        [HttpGet("google-response")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var claims = await _authenticationService.GoogleResponse();
+            return new JsonResult(claims);
+        }
+        [HttpGet("github-login")]
+        public IActionResult GithbLogin()
+        {
+            var redirectUri = Url.Action("GithbResponse");
+            return _authenticationService.GithubLogin(redirectUri);
+        }
+        [HttpGet("github-response")]
+        public async Task<IActionResult> GithbResponse()
+        {
+            var claims = await _authenticationService.GitHubResponse();
+            return new JsonResult(claims);
+        }
+    }
+}
